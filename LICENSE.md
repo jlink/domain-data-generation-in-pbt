708fd54e8126f6b494383d4c674a426db2667114
Johannes Link <business@johanneslink.net>
2022-01-21 09:46:42 +0100

    Set up project, license and intitial README.md

---
+++ LICENSE.md
@@ -0,0 +1,6 @@
+Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) 
+=======================================================
+
+There are two versions of this license:
+- [The human-readable one](https://creativecommons.org/licenses/by-sa/4.0/)
+- [The legal code]()https://creativecommons.org/licenses/by-sa/4.0/legalcode
---
+++ src/test/java/ddg/jqwik/JqwikExample.java
@@ -0,0 +1,16 @@
+package ddg.jqwik;
+
+import net.jqwik.api.*;
+
+class JqwikExample {
+	@Property
+	void stringsAndEvenInts(@ForAll String anyString, @ForAll("evenNumbers") int anEvenInt) {
+		System.out.println("anyString: " + anyString);
+		System.out.println("anEvenInt: " + anEvenInt);
+	}
+
+	@Provide
+	Arbitrary<Integer> evenNumbers() {
+		return Arbitraries.integers().filter(i -> i % 2 == 0);
+	}
+}
